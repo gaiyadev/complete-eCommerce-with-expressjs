026d88db75e823dcace38065d45f222967c6e047
Gaiya Obed <gaiyaobed94@gmail.com>
2020-03-23 11:35:00 +0100

    setting up product views in main site

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,43 @@
+const Product = require('../models/product');
 var express = require('express');
 var router = express.Router();
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('pages/index', { title: 'Welcome to NodeStore' });
+router.get('/', (req, res, next) => {
+  try {
+    Product.find({}, (err, product) => {
+      if(err) throw err;
+      res.render('pages/index', { title: 'Welcome to NodeStore', product: product });
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 
 // Women Fashion Page
 router.get('/women', (req, res, next) =>  {
-  res.render('pages/women', { title: 'Women Section' });
+  try {
+    Product.find({ProductCategory: 'Women Fashion'}, (err, product) => {
+      if(err) throw err;
+      res.render('pages/women', { title: 'Women Section', product: product });
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 // Men Fashion Page
 router.get('/men', (req, res, next) => {
-  res.render('pages/men', { title: 'Men Section' });
+  try {
+    Product.find({ProductCategory: 'Men Fashion'}, (err, product) => {
+      if (err) throw err;
+      res.render('pages/men', { title: 'Men Section', product: product });
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 // Men Phone and Tablets Page
@@ -25,23 +47,49 @@ router.get('/phoneandTablets', (req, res, next) => {
 
 // Men Jewelry Page
 router.get('/jewelry', (req, res, next) => {
-  res.render('pages/jewelry', { title: 'Jewelry Section' });
+  try {
+    Product.find({ProductCategory: 'Jewlyries'}, (err, product) => {
+      if(err) throw err;
+      res.render('pages/jewelry', { title: 'Jewelry Section', product: this.product });
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 // Men Shoes Page
 router.get('/shoes', (req, res, next) => {
-  res.render('pages/shoes', { title: 'Shoes Section' });
+  try {
+    Product.find({ProductCategory: 'Shoes'}, (err, product) => {
+      if(err) throw err;
+      res.render('pages/shoes', { title: 'Shoes Section', product: product });
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 // Men Contact Page
 router.get('/contact', (req, res, next) => {
-  res.render('pages/contact', { title: 'Contac Us' });
+  try {
+    res.render('pages/contact', { title: 'Contac Us' });
+  } catch (err) {
+    console.log(err); 
+  }
 });
 
 
 //  Product Page
 router.get('/product', (req, res, next) => {
-  res.render('pages/product', { title: 'Product page' });
+  try {
+    Product.find({}, (err, product) => {
+      if(err) throw err;
+      res.render('pages/product', { title: 'Product page', product: product });
+    });
+  } catch (err) {
+    console.log(err);
+    
+  }
 });
 
 
