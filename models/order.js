30b7e0ded129b9fc5af9fdc996f6d11eede7af15
Gaiya M Obed <gaiyaobed94@gmail.com>
2020-03-30 19:10:14 +0100

    new feature added

--- models/order.js
+++ models/order.js
@@ -76,10 +76,10 @@ const OrderSchema = new mongoose.Schema({
 const Order = mongoose.model('Order', OrderSchema);
 module.exports = Order;
 
-// Function to create new Admin    
+// Function to create new Order    
 module.exports.createOrder = (newOrder, callback) => {
-    newOrder.save(callback); //create New Admin
-    // Send a mail Register
+    newOrder.save(callback); //create New Order
+    // Send a mail on order
     let transporter = nodemailer.createTransport({
         service: 'Gmail',
         auth: {
@@ -90,7 +90,7 @@ module.exports.createOrder = (newOrder, callback) => {
 
     let mailOptions = {
         from: process.env.APP_EMAIL,
-        to: newOrder.Email, //admin mail
+        to: newOrder.Email, //customer email
         subject: 'Your Order',
         text: "Dear Esteem Customerr.",
         html: `<p>You got a new order submission with the following details...</p>
